c5c4c55e438b83666d83a41f55ff6e755dbddf2c
varunkumarsundaram <nivedhapriya@gmail.com>
2025-09-27 22:14:54 +0930

    added tsc watch

--- src/tests/test_suite.spec.ts
+++ src/tests/test_suite.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "node_modules/@playwright/test/index";
 
-test('test1', async ({ page, browser }) => {
+test('test3', async ({ page, browser }) => {
   //const context = await browser.newContext({ viewport: { width: 1280, height: 720 } });
   //const headfulPage = await context.newPage();
   await page.goto('https://www.google.com');
@@ -9,7 +9,7 @@ test('test1', async ({ page, browser }) => {
   expect(title).toBe('Google');
 });
 
-test('test2', async ({ page, browser }) => {
+test('test3', async ({ page, browser }) => {
   //const context = await browser.newContext({ viewport: { width: 1280, height: 720 } });
   //const headfulPage = await context.newPage();
   await page.goto('https://www.apple.com');
